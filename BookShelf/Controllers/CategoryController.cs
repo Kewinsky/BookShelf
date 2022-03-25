8926fc8d7c81eb5b5bc185f28b80d35f23409e62
Kewin Tao Anh <kewin.taoanh@gmail.com>
2022-03-25 09:33:09 +0100

    dodanie bazy danych. dodanie widoku Index Category

---
+++ BookShelf/Controllers/CategoryController.cs
@@ -0,0 +1,21 @@
+﻿using BookShelf.Data;
+using BookShelf.Models;
+using Microsoft.AspNetCore.Mvc;
+
+namespace BookShelf.Controllers
+{
+    public class CategoryController : Controller
+    {
+        private readonly ApplicationDbContext _db;
+
+        public CategoryController(ApplicationDbContext db)
+        {
+            _db = db;
+        }
+        public IActionResult Index()
+        {
+            IEnumerable<Category> objCategoryList = _db.Categories;
+            return View(objCategoryList);
+        }
+    }
+}
---
+++ BookShelf/Data/ApplicationDbContext.cs
@@ -0,0 +1,15 @@
+﻿using BookShelf.Models;
+using Microsoft.EntityFrameworkCore;
+
+namespace BookShelf.Data
+{
+    public class ApplicationDbContext : DbContext
+    {
+        public ApplicationDbContext(DbContextOptions<ApplicationDbContext> options) : base(options)
+        {
+
+        }
+
+        public DbSet<Category> Categories { get; set; }
+    }
+}
